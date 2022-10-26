49cfd404f9e24a3865e44e0adcedd32c429ff02b
Aadi-kashyav <101461343+Aadi-kashyav@users.noreply.github.com>
2022-10-26 13:40:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 nose_x = 0;
 nose_y = 0;
 function preload() {
-    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png')
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 function setup() {
     canvas = createCanvas(300,300);
@@ -14,22 +14,20 @@ function setup() {
 }
 function draw() {
     image(video,0,0,300,300);
-    fill(255,0,0);
-    stroke(255,0,0);
-    image(clown_nose,nose_x,nose_y,30,30);
+    image(mustache,nose_x,nose_y,50,30)
 }
 function take_snapshot() {
     save('MyfilterImage.png');
 }
-classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+    classifier = ml5.imageClassifier('MobileNet',modelLoaded);
 function modelLoaded() {
     console.log('Posenet is Initialized')
 }
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-        nose_x = results[0].pose.nose.x - 14;
-        nose_y = results[0].pose.nose.y - 10;
+        nose_x = results[0].pose.nose.x - 18;
+        nose_y = results[0].pose.nose.y + 7;
         console.log("nose x = " + nose_x);
         console.log("nose y = " +nose_y);
     }
